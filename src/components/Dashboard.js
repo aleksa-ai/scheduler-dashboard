2c019cf130951343641d84373ced3a0514bd5636
Aleksa Gavrilovic <lks_g@live.ca>
2021-01-28 23:23:04 -0500

    Implement Binding this & Toggle focus

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -35,10 +35,10 @@ class Dashboard extends Component {
   };
 
   selectPanel(id) {
-    this.setState({
-     focused: id
-    });
-   }
+    this.setState((previousState) => ({
+      focused: previousState.focused !== null ? null : id,
+    }));
+  }
 
   render() {
     const dashboardClasses = classnames("dashboard", {
@@ -60,7 +60,7 @@ class Dashboard extends Component {
           id={panel.id}
           label={panel.label}
           value={panel.value}
-          onSelect={this.selectPanel}
+          onSelect={event => this.selectPanel(panel.id)}
         />
       ));
 
